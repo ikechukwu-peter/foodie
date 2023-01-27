3428cac94f73d2856b7936b32970cb6f39d42f81
ikechukwu-peter <pitiyke@gmail.com>
2023-01-27 13:32:16 +0100

    home test

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { AddRecipe } from "./add-recipe";
-import { Home } from "./home";
 import { MyRecipes } from "./my-recipes";
 import { More } from "./more";
 
-export { Home, AddRecipe, MyRecipes, More };
+export { AddRecipe, MyRecipes, More };
